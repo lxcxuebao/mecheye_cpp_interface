d243a110aefc3f450c28835949466f7b8c4a2080
maoyi-peter <mao.yi@mech-mind.net>
2020-08-19 15:19:08 +0800

    add system pause

--- sample/sample1_parameter.cpp
+++ sample/sample1_parameter.cpp
@@ -7,22 +7,20 @@ int sample::parameter()
 	CameraClient camera;
 	std::string error;
 	// Camera ip should be modified to actual ip address.
-	const std::string cameraIp = "192.168.3.146";
+	const std::string cameraIp = "192.168.3.76";
 	if (!camera.connect(cameraIp)) return -1; //return -1 if connection to camera fails
-	std::string para = "camera2DExpTime"; //parameter name you want to get or set
+	std::string para = "exposure1"; //parameter name you want to get or set
 	std::cout << "Camera IP: " << camera.getCameraIp() << std::endl
 		<< "Camera ID: " << camera.getCameraId() << std::endl
 		<< "Version: " << camera.getCameraVersion() << std::endl; //get and print some information about camera device
-
-	std::cout<<camera.setParamater(para, 10)<<std::endl;
+	
+	std::cout << camera.setParamater(para, 10) << std::endl;
 	//unit for exposure time is ms, return value of setParameter is string.If successful, the return string will be empty, otherwise it prints error message.
-
-	std::cout<<camera.getParameter(para, error)<<std::endl;
-	if(!error.empty())
-	std::cout << "ERROR:" << error << std::endl;
+	std::cout<<"The value is "<<camera.getParameter(para, error)<<std::endl;
+	if(!error.empty())  
+		std::cout << "ERROR:" << error << std::endl;
 	//get the value of a parameter. The "error" would be empty if successful.
 
-	
 
 	return 0;
 }
--- sample/sample2_ImgAndCloud.cpp
+++ sample/sample2_ImgAndCloud.cpp
@@ -9,7 +9,7 @@ int sample::ImgAndCloud()
 	CameraClient camera;
 	std::string error;
 	// Camera ip should be modified to actual ip address.
-	const std::string cameraIp = "192.168.3.146";
+	const std::string cameraIp = "192.168.3.76";
 	if (!camera.connect(cameraIp)) return -1; //return -1 if connection to camera fails
 
 	std::cout << "Camera IP: " << camera.getCameraIp() << std::endl
